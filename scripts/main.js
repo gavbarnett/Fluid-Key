ef9e5a270e60f547810b20568e96e0004e39432c
Gavin Barnett <gavbarnett@gmail.com>
2022-03-01 22:52:54 +0000

    Adding a canvas area to test on

--- scripts/main.js
+++ scripts/main.js
@@ -3011,7 +3011,15 @@ function predict(input_str)
 	console.log(suggestions)
 }
 
+function KeyboardInit()
+{
+	var canvas = document.getElementById("Keyboard-Canvas");
+	var ctx = canvas.getContext("2d");
+	ctx.font = "30px Arial";
+	ctx.fillText("H", canvas.clientWidth/10, canvas.clientHeight/2);
+}
+
 function main()
 {
-	
+	KeyboardInit()
 }
