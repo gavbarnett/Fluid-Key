ee26d3578789f1f326f235c3a51fb415b1c41e0c
Gavin Barnett <gavbarnett@gmail.com>
2022-03-11 22:55:22 +0000

    Adding css styles for basic messaging app look

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 
 function KeyboardInit()
 {
-	var canvas = document.getElementById("Keyboard-Canvas");
+	var canvas = document.getElementById("keyboard-canvas");
 	var ctx = canvas.getContext("2d");
 	ctx.font = "30px Arial";
 	ctx.fillText("H", canvas.clientWidth/10, canvas.clientHeight/2);
