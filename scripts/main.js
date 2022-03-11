fa79426f58e8c0c76caa1963104a3c7fa4f6a684
Gavin Barnett <gavbarnett@gmail.com>
2022-03-11 23:19:21 +0000

    testing import of predict.js (not included)
    checked js can import predict.js json data. This is simply a js file with "predict = " appended to the start of the json file output by ninja_gram.py.

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,8 @@ function KeyboardInit()
 	var canvas = document.getElementById("keyboard-canvas");
 	var ctx = canvas.getContext("2d");
 	ctx.font = "30px Arial";
-	ctx.fillText("H", canvas.clientWidth/10, canvas.clientHeight/2);
+	ctx.fillStyle = "#999999";
+	ctx.fillText(predict["__ngrams__"]["__letters__"]["a"]["__count__"], canvas.clientWidth/10, canvas.clientHeight/2);
 }
 
 function main()
